ded4c471f5d5a4b0a54b150df7bb0828d931811a
Philip <philip@wiemermail.de>
2025-04-01 10:32:32 +0200

    Neue Klassen für Schrauben und MetallSchraube hinzugefügt
    - `MetallSchraube.cs`: Einführung der Klasse `MetallSchraube` mit einem Konstruktor.
    - `Schrauben.cs`: Einführung der Klasse `Schrauben` mit geschützten Feldern und Getter-Methoden.
    - `ViewModel.cs`: Einführung der leeren Klasse `ViewModel`.

---
+++ Schrauben_Schutte/MetallSchraube.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Schrauben_Schutte
+{
+    class MetallSchraube
+    {
+        public MetallSchraube(string n,string a,string l,string b,double m,double p,string v)
+        {
+
+        }
+    }
+}
---
+++ Schrauben_Schutte/Schrauben.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Schrauben_Schutte
+{
+    class Schrauben
+    {
+        protected string schraubenName;
+        protected string schraubenArt;
+        protected string schraubenLaenge;
+        protected double schraubenMasse;
+        protected string schraubenBild;
+        protected double schraubenPreis;
+        protected string schraubenVerdrillung;
+        protected Schrauben()
+        {
+     
+        }
+        public string getName
+        {
+            get { return schraubenName; }
+        }
+        public string getArt
+        {
+            get { return schraubenArt; }
+        }
+        public string getLaenge
+        {
+            get { return schraubenLaenge; }
+        }
+        public double getMasse
+        {
+            get { return schraubenMasse; }
+        }
+        public string getBild
+        {
+            get { return schraubenBild; }
+        }
+        public double getPreis
+        {
+            get { return schraubenPreis; }
+        }
+        public string getVerdrillung
+        {
+            get { return schraubenVerdrillung; }
+        }
+
+
+    }
+}
