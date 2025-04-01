f4efe608210093098eef2a0be934c5c5cfe61ce0
Philip <philip@wiemermail.de>
2025-04-01 10:45:28 +0200

    View Model ergänzen

--- Schrauben_Schutte/MetallSchraube.cs
+++ Schrauben_Schutte/MetallSchraube.cs
@@ -6,11 +6,17 @@ using System.Threading.Tasks;
 
 namespace Schrauben_Schutte
 {
-    class MetallSchraube
+    class MetallSchraube : Schrauben
     {
-        public MetallSchraube(string n,string a,string l,string b,double m,double p,string v)
+        public MetallSchraube(string n,string a,string l,string b,double m,double p,string v) : base()
         {
-
+            schraubenName = n;
+            schraubenMasse = m;
+            schraubenPreis = p;
+            schraubenArt = a;
+            schraubenLaenge = l;
+            schraubenBild = b;
+            schraubenVerdrillung = v;
         }
     }
 }
--- Schrauben_Schutte/ViewModel.cs
+++ Schrauben_Schutte/ViewModel.cs
@@ -8,5 +8,14 @@ namespace Schrauben_Schutte
 {
     class ViewModel
     {
+        List<Schrauben> schraubenListe = new List<Schrauben>();
+        public ViewModel()
+        {
+            schraubenListe.Add(new MetallSchraube("M1", "Metall", "10mm", "Bild1", 0.1, 0.1, "Verdrillung1"));
+        }
+        public List<Schrauben> getListe
+        {
+            get { return schraubenListe; }
+        }
     }
 }
